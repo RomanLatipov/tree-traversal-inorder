39d684089f4c5a2f27d762e1836bda6841b1d315
RomanLatipov <romanlatipov@gmail.com>
2024-07-14 21:26:28 -0400

    complete tree-traversal-inorder lab

--- javascript/tree_traversal_inorder.js
+++ javascript/tree_traversal_inorder.js
@@ -7,7 +7,18 @@ class Node {
 }
 
 function treeTraversalInorder(root) {
-  // type your code here
+  const result = [];
+
+  function backtrack(root, result) {
+    if (root === null) return [];
+
+    backtrack(root.left, result);
+    result.push(root.value);
+    backtrack(root.right, result);
+
+    return result;
+  }
+  return backtrack(root, result);
 }
 
 if (require.main === module) {
@@ -19,6 +30,9 @@ if (require.main === module) {
   console.log("");
 
   root = new Node(10, new Node(0, null, new Node(5)), new Node(20, null, new Node(30)));
+                            // 10
+                          // 0   20
+                        //    5    30
   console.log("Expecting: [0, 5, 10, 20, 30] ");
   console.log(treeTraversalInorder(root));
 }
